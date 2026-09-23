3cd8369e41212100bab3b4fbcce892564960abc2
Tom Gayas <tomgayas26@gmail.com>
2026-09-23 20:53:48 +0800

    g

--- src/components/Confidence.tsx
+++ src/components/Confidence.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import galleryImage1 from '../assets/gallery/optimized/newgallery3.webp';
 import galleryImage2 from '../assets/gallery/optimized/newgallery1.webp';
-import galleryImage3 from '../assets/gallery/optimized/newgallery2.webp';
+import galleryImage3 from '../assets/gallery/optimized/newgallery2.png';
 import galleryImage4 from '../assets/gallery/optimized/gallery4new.webp';
 
 const galleryImages = [
